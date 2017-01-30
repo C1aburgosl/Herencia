38338ec2119b1f0103cc162f9fa1cdd279b7622c
angelicaB <angelicaburgosabl@gmail.com>
2017-01-30 18:53:54 +0100

    Primer commit

---
+++ src/package1/Cuadrado.java
@@ -0,0 +1,31 @@
+package package1;
+
+public class Cuadrado extends Figura {
+
+	private double lado;
+	
+	
+	public Cuadrado(double l){
+		
+		lado = l;
+		
+		//Atributo de Figura
+		numeroDeLados = 4;
+		
+		//Variable global
+		numeroDeFiguras++;
+	}
+	
+	public double area(){
+		return lado*lado;
+	}
+	
+	public String toString(){
+		return "Valores del Cuadrado\n" +
+			   "-----------------------\n" +
+			   "Lado = " + lado + "\n" +
+			   "area = " + area() + "\n" +
+			   "Numero de Lados = " + numeroDeLados + "\n" +
+			   "Figuras Totales = " + numeroDeFiguras;
+	}
+}
